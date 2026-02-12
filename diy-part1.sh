9ccf6c51a18d4aeccaa0762545cd56cbb5b1d944
JohnnyDu0815 <39909450+JohnnyDu0815@users.noreply.github.com>
2026-02-13 03:38:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-echo 'src-git JohnnyDu0815 https://github.com/JohnnyDu0815/small-package' >>feeds.conf.default
-
+#echo 'src-git JohnnyDu0815 https://github.com/JohnnyDu0815/small-package' >>feeds.conf.default
+echo 'src-git JohnnyDu0815 https://github.com/JohnnyDu0815/openwrt-packages' >>feeds.conf.default
 #chmod -R 755 ./package/luci-app-adguardhome/*
